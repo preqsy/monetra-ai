555350f80e8298be34aa6825096e9d1f0307da30
Preqsy <obbyprecious24@gmail.com>
2026-01-08 18:08:42 +0100

    feat: ran NLService on startup

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
+from services.nl import get_nl_service
 from api import router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print(f"Starting NLServic...")
+    get_nl_service()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
--- services/nl.py
+++ services/nl.py
@@ -1,5 +1,7 @@
+from functools import lru_cache
+from typing import TYPE_CHECKING
+
 from qdrant_client import QdrantClient
-from api.models import NLRequest, PriceFormat
 from nl.models import NLResolveRequest
 from nl.nl_query_processor import NLQueryResolver
 from rag.embedder import OllamaEmbedder
@@ -25,13 +27,13 @@ class NLService:
 
         self.llm = NLQueryResolver(retriever=self.retriever)
 
-    async def resolve_user_query(self, data_obj: NLRequest):
+    async def resolve_user_query(self, data_obj: "NLRequest"):
         req = NLResolveRequest(**data_obj.model_dump())
         rsp = self.llm.resolve_nl(req)
 
         return rsp
 
-    async def format_price_with_category(self, data_obj: PriceFormat):
+    async def format_price_with_category(self, data_obj: "PriceFormat"):
         return self.llm.format_price_query(
             amount=data_obj.amount,
             category=data_obj.category,
@@ -39,5 +41,10 @@ class NLService:
         )
 
 
+@lru_cache(maxsize=1)
 def get_nl_service():
     return NLService()
+
+
+if TYPE_CHECKING:
+    from api.models import NLRequest, PriceFormat
