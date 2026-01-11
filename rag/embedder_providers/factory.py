3b4c1f010fb9c3900af8e545608d0bbe820d049b
Preqsy <obbyprecious24@gmail.com>
2026-01-11 20:06:52 +0100

    removed imports

--- rag/embedder_providers/factory.py
+++ rag/embedder_providers/factory.py
@@ -1,6 +1,4 @@
 from rag.embedder_providers.base import EmbedderABC
-from rag.embedder_providers.google_model import GoogleEmbedder
-from rag.embedder_providers.ollama_model import OllamaEmbedder
 from config import settings
 
 
@@ -8,11 +6,17 @@ def get_embedding_model() -> EmbedderABC:
 
     if settings.EMBEDDING_MODEL == "ollama":
         print(f"Using model: {settings.EMBEDDING_MODEL}")
+        from rag.embedder_providers.ollama_model import OllamaEmbedder
+
         return OllamaEmbedder()
 
     elif settings.EMBEDDING_MODEL == "google":
         print(f"Using model: {settings.EMBEDDING_MODEL}")
+        from rag.embedder_providers.google_model import GoogleEmbedder
+
         return GoogleEmbedder()
 
     else:
+        from rag.embedder_providers.ollama_model import OllamaEmbedder
+
         return OllamaEmbedder()
