02c5bf57fbbd952b950334029964022b03fc661a
Preqsy <obbyprecious24@gmail.com>
2026-01-12 23:05:57 +0100

    refactor: Optimize imports in factory.py and update Dockerfile for poetry installation

--- nl/llm_providers/factory.py
+++ nl/llm_providers/factory.py
@@ -1,13 +1,15 @@
 from nl.llm_providers.base import LLMProvider
-from nl.llm_providers.groq_provider import GroqProvider
-from nl.llm_providers.ollama_provider import OllamaProvider
 
 
 def get_llm_provider(temperature: float, llm_provider: str = "ollama") -> LLMProvider:
     if llm_provider == "ollama":
+        from nl.llm_providers.ollama_provider import OllamaProvider
+
         # For local development and to reduce cloud cost
         return OllamaProvider(temperature=temperature)
     elif llm_provider == "groq":
+        from nl.llm_providers.groq_provider import GroqProvider
+
         # For prod.... TODO: Use a better free LLM Model.
         return GroqProvider(temperature=temperature)
     else:
